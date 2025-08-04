d697d51252b2f609d46b524042f02892cf96a2d5
IgorFernandes <igorfernandestec@gmail.com>
2025-08-04 19:29:55 -0300

    base snippets added

---
+++ base.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define FOR(i, a, b) for (int i = a; i <= b; ++i)
+#define RFOR(i, b, a) for (int i = b; i >= a; --i)
+#define REP(i, N) for (int i = 0; i < N; ++i)
+#define pb push_back
+#define mp make_pair
+#define f first
+#define s second
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+#define dbg(x) cout << #x << " = " << x << endl
+#define print(x) cout << x << "\n"
+
+typedef long long ll;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+using namespace std;
+
+int main() { _
+
+    
+}
